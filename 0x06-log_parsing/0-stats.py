1f43355ea5b1742e2d8e510df7d2802e1be79944
Cristhian-Carbonell <alexcarbo8809@gmail.com>
2021-06-09 15:13:56 -0500

    0x06

---
+++ 0x06-log_parsing/0-stats.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+import sys
+
+lines = sys.stdin
+count = 1
+size = []
+code = {"200": 0,
+        "301": 0,
+        "400": 0,
+        "401": 0,
+        "403": 0,
+        "404": 0,
+        "405": 0,
+        "500": 0}
+
+for line in lines:
+    if count == 10:
+        total_size = sum(size)
+        print("File size: {}".format(total_size))
+        size.clear()
+        size.append(total_size)
+        for i in code:
+            if code[i] != 0:
+                print("{}: {}".format(i, code[i]))
+        count = 0
+
+    str_line = line[-8:]
+    print(str_line)
+    size.append(int(str_line[4:]))
+    print(size)
+    str_code = str_line[:3]
+    if str_code in code:
+        code[str_code] += 1
+    count += 1
