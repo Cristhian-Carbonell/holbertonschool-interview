60d29c9fb7f7c1b6e55c4b78a4c3e58cec0d459e
Cristhian-Carbonell <alexcarbo8809@gmail.com>
2021-06-10 17:40:07 -0500

    update file 0-check_cycle.c

--- 0x07-linked_list_cycle/0-check_cycle.c
+++ 0x07-linked_list_cycle/0-check_cycle.c
@@ -2,23 +2,25 @@
 #include <stdio.h>
 
 /**
- * check_cycle - checks if a singly linked list has a cycle in it
- * @list:
- *
- * Return: 0 if there is no cycle, 1 if there is a cycle
- */
+* check_cycle - checks if a singly linked list has a cycle in it
+* @list:
+*
+* Return: 0 if there is no cycle, 1 if there is a cycle
+*/
 int check_cycle(listint_t *list)
 {
 	listint_t *node = list->next;
 
-	if (list == NULL)
+	if (!list)
 		return (0);
 
-	while (node != NULL && node != list)
+	while (node)
+	{
+		if (node == list)
+			return (1);
 		node = node->next;
-	if (node == NULL)
-		return (0);
+	}
 
-	return (1);
+	return (0);
 }
 
