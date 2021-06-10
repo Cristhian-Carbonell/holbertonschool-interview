9572cf46f8c551c76146709e43457428c17b3510
Cristhian-Carbonell <alexcarbo8809@gmail.com>
2021-06-10 17:53:00 -0500

    update file 0-check_cycle.c

--- 0x07-linked_list_cycle/0-check_cycle.c
+++ 0x07-linked_list_cycle/0-check_cycle.c
@@ -3,7 +3,7 @@
 
 /**
 * check_cycle - checks if a singly linked list has a cycle in it
-* @list:
+* @list: list to check
 *
 * Return: 0 if there is no cycle, 1 if there is a cycle
 */
@@ -21,8 +21,8 @@ int check_cycle(listint_t *list)
 			node = node->next->next;
 		else
 			node = node->next;
-
 		current = current->next;
+
 		if (node == current)
 			return (1);
 	}
