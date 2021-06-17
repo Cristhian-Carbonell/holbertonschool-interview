d3820bf07b591121d06b4d9256cc459ce1eff7f1
Cristhian-Carbonell <alexcarbo8809@gmail.com>
2021-06-17 15:39:57 -0500

    Palindrome unsigned integer

--- 0x08-palindrome_integer/0-is_palindrome.c
+++ 0x08-palindrome_integer/0-is_palindrome.c
@@ -1,7 +1,10 @@
 #include "palindrome.h"
 
 /**
+ * is_palindrome - checks whether or not a given unsigned integer is a palindrome
+ * @n: is the number to be checked
  *
+ * Return: 1 if n is a palindrome, 0 otherwise
  */
 int is_palindrome(unsigned long n)
 {
