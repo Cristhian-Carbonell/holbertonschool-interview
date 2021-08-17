450c41b819c17b60e6c7c79720d01a73fef589f6
Cristhian-Carbonell <alexcarbo8809@gmail.com>
2021-08-17 14:03:11 -0500

    Star Wars Characters

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -3,12 +3,21 @@ const id = process.argv.slice(2);
 const url = `https://swapi-api.hbtn.io/api/films/${id[0]}/`;
 
 const request = require('request');
-request(url, function (error, response, body) {
+request(url, async function (error, response, body) {
+  if (error) {
+    return console.error(error);
+  }
   const bodyJson = JSON.parse(body);
   for (const characters of bodyJson.characters) {
-    request(characters, function (error, response, body) {
-      const bdJson = JSON.parse(body);
-      console.log(bdJson.name);
+    await new Promise(function (resolve, reject) {
+      request(characters, function (error, response, body) {
+        if (error) {
+          return console.error(error);
+        }
+        const bdJson = JSON.parse(body);
+        console.log(bdJson.name);
+        resolve();
+      });
     });
   }
 });
