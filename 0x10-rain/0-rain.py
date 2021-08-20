bd2231b566c51f5bfde58f42b5c8a740ab1de728
Cristhian-Carbonell <alexcarbo8809@gmail.com>
2021-08-19 22:44:08 -0500

    Rain

---
+++ 0x10-rain/0-rain.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+"""rain module
+"""
+
+
+def rain(walls):
+    """calculate how many square units of water will be
+    retained after it rains
+    """
+    left_wall = 0
+    right_wall = 0
+    result = 0
+    index = walls.index(0)
+    if index == walls[0]:
+        i = 1
+        while i <= len(walls) - 1:
+            if walls[i] == 0:
+                left_wall = walls[i - 1]
+                right_wall = walls[i + 1]
+            if left_wall < right_wall:
+                result += left_wall
+                left_wall = 0
+                right_wall = 0
+            i += 1
+        return result
+    else:
+        count = 0
+        auxiliary = 0
+        i = 0
+        left_wall = walls[i]
+        while i <= len(walls) - 2:
+            if walls[i] == 0:
+                count += 1
+                right_wall = walls[i + 1]
+            else:
+                count = 0
+            if left_wall < right_wall:
+                auxiliary = left_wall * count
+                result += auxiliary
+                count = 0
+                left_wall = right_wall
+            else:
+                auxiliary = right_wall * count
+                result += auxiliary
+            i += 1
+        return result
