ac250b5624b1652cc7e1f86196afd7d39adf3cba
Cristhian-Carbonell <alexcarbo8809@gmail.com>
2021-08-26 19:18:30 -0500

    Heap sort

--- 0x11-heap_sort/0-heap_sort.c
+++ 0x11-heap_sort/0-heap_sort.c
@@ -5,42 +5,36 @@
  * @xp: values pointed by xp
  * @yp:values pointed by yp
  */
-void swap(int *xp, int *yp)
+void swap(int *xp, int *yp, int *array, size_t size)
 {
 	int temp = *xp;
 	*xp = *yp;
 	*yp = temp;
+	print_array(array, size);
 }
 
 /**
  * sift_down - To heapify a subtree rooted with node i which is
  * an index in arr[]. n is size of heap
  * @array: array of values
- * @size: aray size
+ * @n: array size
  * @end: root element value
+ * @size: size
  */
-void sift_down(int *array, int size, int end)
+void sift_down(int *array, int n, int end, size_t size)
 {
 	int root = end;
 	int left = 2 * end + 1;
 	int right = 2 * end + 2;
-	int n = 0;
 
-	if (left < size && array[left] > array[root])
+	if (left < n && array[left] > array[root])
 		root = left;
-	if (right < size && array[right] > array[root])
+	if (right < n && array[right] > array[root])
 		root = right;
 	if (root != end)
 	{
-		swap(&array[end], &array[root]);
-		if (size < 10)
-		{
-			n = 10;
-			print_array(array, n);
-		}
-		else
-			print_array(array, size);
-		sift_down(array, size, root);
+		swap(&array[end], &array[root], array, size);
+		sift_down(array, n, root, size);
 	}
 }
 
@@ -57,11 +51,10 @@ void heap_sort(int *array, size_t size)
 		return;
 
 	for (i = size / 2 - 1; i >= 0; i--)
-		sift_down(array, size, i);
+		sift_down(array, size, i, size);
 	for (i = size - 1; i > 0; i--)
 	{
-		swap(&array[0], &array[i]);
-		print_array(array, size);
-		sift_down(array, i, 0);
+		swap(&array[0], &array[i], array, size);
+		sift_down(array, i, 0, size);
 	}
 }
