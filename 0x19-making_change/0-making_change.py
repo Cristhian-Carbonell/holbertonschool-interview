b7a2ae7eb9eb7e5664b7e84a0d28c00466a95515
Cristhian-Carbonell <alexcarbo8809@gmail.com>
2022-02-02 20:41:38 -0500

    0. Change comes from within

--- 0x19-making_change/0-making_change.py
+++ 0x19-making_change/0-making_change.py
@@ -3,6 +3,7 @@
     find minimum of coins to make a given change total"""
 import sys
 
+
 def makeChange(coins, total):
 
     if (total <= 0):
@@ -11,26 +12,26 @@ def makeChange(coins, total):
     size = len(coins)
     return minCoins(coins, size, total)
 
- 
+
 def minCoins(coins, size, total):
-     
+
     table = [0 for i in range(total + 1)]
- 
+
     table[0] = 0
- 
+
     for i in range(1, total + 1):
         table[i] = sys.maxsize
- 
+
     for i in range(1, total + 1):
-         
+
         for j in range(size):
             if (coins[j] <= i):
                 sub_res = table[i - coins[j]]
                 if (sub_res != sys.maxsize and
-                    sub_res + 1 < table[i]):
+                        sub_res + 1 < table[i]):
                     table[i] = sub_res + 1
-     
+
     if table[total] == sys.maxsize:
         return -1
-       
+
     return table[total]
