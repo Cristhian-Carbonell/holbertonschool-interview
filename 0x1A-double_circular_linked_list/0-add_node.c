c2ffd85a35ee1a846c453aa3149d25f180c73341
Cristhian-Carbonell <alexcarbo8809@gmail.com>
2022-02-10 21:38:51 -0500

    updafile segmentation fault... check malloc's return

--- 0x1A-double_circular_linked_list/0-add_node.c
+++ 0x1A-double_circular_linked_list/0-add_node.c
@@ -13,7 +13,7 @@ List *add_node_end(List **list, char *str)
 
 	if (!head)
 		return (NULL);
-	head->str = strdup(str);
+	head->str = _strdup(str);
 	if (!*list)
 	{
 		head->next = head;
@@ -50,4 +50,37 @@ List *add_node_begin(List **list, char *str)
 	*list = head;
 
 	return (head);
+}
+
+/**
+ * _strdup - returns a pointer to a newly allocated space in memory
+ * @str: character
+ *
+ * Return: a pointer to a new string which is a duplicate of the string
+ */
+char *_strdup(char *str)
+{
+	int i, j;
+	char *ptr;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		;
+	}
+	ptr = malloc((i + 1) * sizeof(char));
+
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+	for (j = 0; j <= i; j++)
+	{
+		ptr[j] = str[j];
+	}
+	ptr[j] = '\0';
+	return (ptr);
 }
