100c0ca06ec9878cb2c93adf75404676e8a4c9a9
Cristhian-Carbonell <alexcarbo8809@gmail.com>
2022-02-24 21:51:36 -0500

    Island Perimeter

--- 0x1C-island_perimeter/0-island_perimeter.py
+++ 0x1C-island_perimeter/0-island_perimeter.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-
+"""function that returns the perimeter
+    of the island described in grid
+"""
 
 def island_perimeter(grid):
-    """function that returns the perimeter
-        of the island described in grid
-    """
+    
     perimeter = 0
     base = len(grid)
     height = len(grid[0]) if base else 0
