9d4685cc156cdb357649bc6da52d9446be243fc6
Cristhian-Carbonell <alexcarbo8809@gmail.com>
2022-03-03 18:47:09 -0500

    Is AVL

--- 0x1D-avl_trees/0-binary_tree_is_avl.c
+++ 0x1D-avl_trees/0-binary_tree_is_avl.c
@@ -21,17 +21,6 @@ int isBSTUtil(const binary_tree_t *node, int min, int max)
 		isBSTUtil(node->right, node->n + 1, max));
 }
 
-/**
- * max - returns maximun of two integers
- * @a: one integer
- * @b: two integer
- *
- * Return: returns maximun of two integers
- */
-int max(int a, int b)
-{
-	return ((a >= b) ? a : b);
-}
 
 /**
  * height - compute the height of a tree.
@@ -42,10 +31,40 @@ int max(int a, int b)
  */
 int height(binary_tree_t *tree)
 {
+	int lh, rh;
+
 	if (!tree)
 		return (0);
 
-	return (1 + max(height(tree->left), height(tree->right)));
+	lh = height(tree->left);
+	rh = height(tree->right);
+	if (lh >= rh)
+		return (1 + lh);
+	else
+		return (1 + rh);
+}
+
+/**
+ * binary_tree_is_avl - function that checks if a binary tree
+ * is a valid AVL Tree
+ * @tree: is a pointer to the root node of the tree to check
+ *
+ * Return: 1 if tree is a valid AVL Tree, and 0 otherwise or
+ * If tree is NULL, return 0
+ */
+int isAVL(const binary_tree_t *tree)
+{
+	int lh, rh, diff;
+
+	if (!tree)
+		return (1);
+	lh = height(tree->left);
+	rh = height(tree->right);
+	diff = lh > rh ? lh - rh : rh - lh;
+	if (diff <= 1 &&  isAVL(tree->left) && isAVL(tree->right))
+		return (isBSTUtil(tree, INT_MIN, INT_MAX));
+
+	return (0);
 }
 
 /**
@@ -58,22 +77,10 @@ int height(binary_tree_t *tree)
  */
 int binary_tree_is_avl(const binary_tree_t *tree)
 {
-	int lh, rh;
-
 	if (!tree)
-		return (1);
+		return (0);
 
-	lh = height(tree->left);
-	rh = height(tree->right);
-
-	if (abs(lh - rh) <= 1 && binary_tree_is_avl(tree->left) &&
-		binary_tree_is_avl(tree->right))
-	{
-		if (isBSTUtil(tree, INT_MIN, INT_MAX))
-			return (1);
-	}
-
-	return (0);
+	return (isAVL(tree));
 }
 
 /**
